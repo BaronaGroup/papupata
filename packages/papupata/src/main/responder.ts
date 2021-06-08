b47e12f576cc7c791f0f1b0fa53cb6535e78cead
Tapani Haka <tapani.haka@barona.fi>
2021-06-08 10:18:51 +0300

    Remove a breakpoint

--- packages/papupata/src/main/responder.ts
+++ packages/papupata/src/main/responder.ts
@@ -334,7 +334,6 @@ export function responder<
 
       return call
       function applyPathParams(reqParams: TypedQueryToTypes<ParamsType>) {
-        debugger
         const pathWithParams = paramMatchers(params).reduce((currPath, { matcher, name }) => {
           return currPath.replace(matcher, (_, before, after) => {
             return `${before}${encodeURIComponent((reqParams as any)[name])}${after}`
